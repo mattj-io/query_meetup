868030b450276cdc2bdd62ea31c944c897509e12
Matt Jarvis <mjarvis@mesosphere.com>
2017-08-05 11:48:43 +0100

    Fix linting

--- query_meetup.py
+++ query_meetup.py
@@ -5,6 +5,7 @@ Query meetup.com API
 
 import sys
 import time
+import argparse
 import meetup.api
 import yaml
 import xlsxwriter
@@ -37,6 +38,50 @@ def number_in_period(events, period):
             count += 1
     return count
 
+def create_spreadsheet(columns, groups):
+    """
+    Create a spreadsheet from a set of groups and column headers
+    """
+    workbook = xlsxwriter.Workbook('meetup_query.xlsx')
+    worksheet = workbook.add_worksheet()
+    col = 0
+    for item in columns:
+        worksheet.write(0, col, item)
+        col += 1
+    row = 1
+    for group in groups:
+        organizer_url = group.link+"members/"+str(group.organizer['id'])
+        data = [group.name,
+                group.members,
+                group.city,
+                group.country,
+                group.link,
+                group.organizer['name'],
+                organizer_url]
+        col = 0
+        for item in data:
+            worksheet.write(row, col, item)
+            col += 1
+        row += 1
+    workbook.close()
+
+def create_table(columns, groups):
+    """
+    Output a table from a set of groups and column headers
+    """
+    table = PrettyTable(columns)
+    for group in groups:
+        organizer_url = group.link+"members/"+str(group.organizer['id'])
+        data = [group.name,
+                group.members,
+                group.city,
+                group.country,
+                group.link,
+                group.organizer['name'],
+                organizer_url]
+        table.add_row(data)
+    return table
+
 class MSMeetup(object):
     """
     Define class object
@@ -46,15 +91,21 @@ class MSMeetup(object):
             cfg = yaml.load(ymlfile)
         self.api_key = cfg['meetup']['api_key']
         self.search_keys = cfg['meetup']['search_keys']
-        self.name_filter = cfg['meetup']['name_filter']
-        self.member_filter = (cfg['meetup']['member_filter'], cfg['meetup']['min_members'])
-        self.event_filter = (cfg['meetup']['event_filter'], cfg['meetup']['min_events'])
-        self.freq_filter = (cfg['meetup']['freq_filter'], cfg['meetup']['min_freq'])
-        self.period_filter = (cfg['meetup']['period_filter'], cfg['meetup']['period'], cfg['meetup']['period_min'])
+        self.filters = {}
+        self.filters['name_filter'] = [cfg['meetup']['name_filter']]
+        self.filters['member_filter'] = [cfg['meetup']['member_filter'],
+                                         cfg['meetup']['min_members']]
+        self.filters['event_filter'] = [cfg['meetup']['event_filter'],
+                                        cfg['meetup']['min_events']]
+        self.filters['freq_filter'] = [cfg['meetup']['freq_filter'],
+                                       cfg['meetup']['min_freq']]
+        self.filters['period_filter'] = [cfg['meetup']['period_filter'],
+                                         cfg['meetup']['period'],
+                                         cfg['meetup']['period_min']]
         self.locations = {}
         self.outputs = []
         for output in cfg['output']:
-            self.outputs.append(output) 
+            self.outputs.append(output)
         # Horrible nested for loop to load dict of city, country
         for country in cfg['locations']:
             for location in cfg['locations'][country]:
@@ -78,32 +129,33 @@ class MSMeetup(object):
         con = self.connect_to_meetup()
         try:
             search_string = ' OR '.join(self.search_keys)
-            group_info = con.GetFindGroups({'text':search_string,
+            group_info = con.GetFindGroups({'text':search_string, # pylint: disable=no-member
                                             'country':country,
                                             'location':city})
-        except:
-            #FIXME exception handling
-            print 'Could not search for groups'
+        except meetup.exceptions.MeetupBaseException as err:
+            print 'Could not search for groups: %s' % err
             sys.exit(1)
-        if self.name_filter:
+        if self.filters['name_filter'][0]:
             group_info = self.filter_on_name(group_info)
-        if self.member_filter[0]:
+        if self.filters['member_filter'][0]:
             group_info = self.filter_on_members(group_info)
-        if self.event_filter[0]:
+        if self.filters['event_filter'][0]:
             group_info = self.filter_on_events(group_info)
-        if self.freq_filter[0]:
+        if self.filters['freq_filter'][0]:
             group_info = self.filter_on_freq(group_info)
+        if self.filters['period_filter'][0]:
+            group_info = self.filter_on_period(group_info)
         return group_info
 
-    def get_member(self, id):
+    def get_member(self, member_id):
         """
         Retrieve a members details
         """
         con = self.connect_to_meetup()
         try:
-            res = con.GetMember(id)
-        except:
-            print "Could not get member"
+            res = con.GetMember(member_id) # pylint: disable=no-member
+        except meetup.exceptions.MeetupBaseException as err:
+            print "Could not get member %s" % err
             sys.exit(1)
         return res
 
@@ -111,14 +163,13 @@ class MSMeetup(object):
         """
         Get past events for a group
         """
-        # FIXME exception handling
         con = self.connect_to_meetup()
         try:
-            res = con.GetEvents({'group_id': group.id,
+            res = con.GetEvents({'group_id': group.id, # pylint: disable=no-member
                                  'group_urlname': group.urlname,
                                  'status': 'past'})
-        except:
-            print "Could not get events"
+        except meetup.exceptions.MeetupBaseException as err:
+            print "Could not get events %s" % err
             sys.exit(1)
         return res.results
 
@@ -129,15 +180,15 @@ class MSMeetup(object):
         """
         name_matches = [group for group in groups
                         if any(key.lower() in group.name.lower()
-                        for key in self.search_keys)]
+                               for key in self.search_keys)]
         return name_matches
 
     def filter_on_members(self, groups):
         """
         Return a filtered set of groups based on number of members
         """
-        mem_filter = [group for group in groups 
-                      if group.members > self.member_filter[1]]
+        mem_filter = [group for group in groups
+                      if group.members > self.filters['member_filter'][1]]
         return mem_filter
 
     def filter_on_events(self, groups):
@@ -145,15 +196,16 @@ class MSMeetup(object):
         Return a filtered set of groups based on minimum number of events
         """
         num_event_filter = [group for group in groups
-                            if len(self.get_past_events(group)) > self.event_filter[1]]
+                            if len(self.get_past_events(group)) > self.filters['event_filter'][1]]
         return num_event_filter
 
-    def filter_on_period(self, period):
+    def filter_on_period(self, groups):
         """
         Return a filtered set of groups based on events in past configurable period
         """
         period_event_filter = [group for group in groups
-                              if number_in_period(group, self.period[1]) > self.period[2]]
+                               if number_in_period(group, self.filters['period_filter'][1])
+                               > self.filters['period_filter'][2]]
         return period_event_filter
 
     def filter_on_freq(self, groups):
@@ -161,50 +213,21 @@ class MSMeetup(object):
         Return a filtered set based on a configurable past event frequency
         """
         event_freq_filter = [group for group in groups
-                             if event_frequency(self.get_past_events(group)) < self.freq_filter[1]]
+                             if event_frequency(self.get_past_events(group))
+                             < self.filters['freq_filter'][1]]
         return event_freq_filter
-
-def create_spreadsheet(columns, groups):
-    """
-    Create a spreadsheet from a set of groups and column headers
-    """
-    workbook = xlsxwriter.Workbook('meetup_query.xlsx')
-    worksheet = workbook.add_worksheet()
-    col = 0
-    for item in columns:
-        worksheet.write(0, col, item)
-        col += 1
-    row = 1
-    for group in groups:
-        organizer_url = group.link+"members/"+str(group.organizer['id'])
-    	data = [group.name, group.members, group.city, group.country, group.link, group.organizer['name'], organizer_url]
-        col = 0
-        for item in data:
-            worksheet.write(row, col, item)
-            col += 1
-        row += 1
-    workbook.close()
-
-def create_table(columns, groups):
-    """
-    Output a table from a set of groups and column headers
-    """
-    table = PrettyTable(columns)
-    for group in groups:
-        organizer_url = group.link+"members/"+str(group.organizer['id'])
-        data = [group.name, group.members, group.city, group.country, group.link, group.organizer['name'], organizer_url]
-        table.add_row(data)
-    return table
-
 
 def main():
     """
     Main execution
     """
-    configfile = 'config.mj'
-    meetup_query = MSMeetup(configfile)
+    parser = argparse.ArgumentParser(description='Query Meetup.com')
+    parser.add_argument('--config', action="store", dest="config", help='configuration file to use')
+    args = parser.parse_args()
+
+    meetup_query = MSMeetup(args.config)
     columns = ['Name', 'Members', 'City', 'Country', 'URL', 'Organizer Name', 'Organizer URL']
-    
+
     res = []
     for city, country in meetup_query.locations.iteritems():
         res += meetup_query.search_via_api(city, country)
